53d9ddecb87a2c1ee8a34105c60c8cde0aedb404
Sander Heinsar <sander.heinsar@gmail.com>
2024-10-17 18:27:09 +0300

    Added relevant tests

--- numberstorage/src/test/java/netsestonia/numberstorage/numbers/NumbersRepositoryTest.java
+++ numberstorage/src/test/java/netsestonia/numberstorage/numbers/NumbersRepositoryTest.java
@@ -2,22 +2,37 @@ package netsestonia.numberstorage.numbers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
-
+import static org.mockito.Mockito.*;
 
 class NumbersRepositoryTest {
 
+    NumbersRepository numbersRepository;
+
+    List<Integer> list = Arrays.asList(1, 2, 3);
 
     @BeforeEach
     public void setUp() {
+        numbersRepository = Mockito.mock(NumbersRepository.class);
+        numbersRepository.create(list);
     }
 
     @Test
     public void findAll() {
         List<NumbersEntity> expectedEntities = Collections.emptyList();
+        when(numbersRepository.findAll()).thenReturn(expectedEntities);
+
+        List<NumbersEntity> entities = numbersRepository.findAll();
+        assert entities.equals(expectedEntities);
     }
 
+    @Test
+    public void create() {
+        numbersRepository.create(list);
+        verify(numbersRepository, times(2)).create(list);
+    }
 }
